fc826c69e2c0174d5f7bcf4cc81126564c015571
Volodymyrkorchagin <161863455+Volodymyrkorchagin@users.noreply.github.com>
2026-06-07 19:21:13 +0200

    Update project

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('writers/', views.writers, name='writers'),
     path('books/', views.books, name='books'),
+
+    path('writers/<str:name>/', views.writer_detail, name='writer_detail'),
+    path('books/<str:title>/', views.books_detail, name='books_detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 def home(request):
     return HttpResponse("Головна сторінка")
@@ -8,3 +9,30 @@ def writers(request):
 
 def books(request):
     return HttpResponse("Топ книг")
+
+
+writers_data = {
+    "Hemingway": "Ернест Гемінґвей — американський письменник, лауреат Нобелівської премії.",
+    "Shakespeare": "Вільям Шекспір — англійський драматург і поет.",
+}
+
+books_data = {
+    "Harry Potter and the philosopher's stone ": "Перша книга по Гаррі Поттеру.",
+    "Harry Potter and the chamber of secrets": "Друга книга по Гаррі Поттеру.",
+}
+
+def writer_detail(request, name):
+    info = writers_data.get(name)
+
+    if info:
+        return HttpResponse(info)
+    else:
+        return redirect('writers')
+
+def books_detail(request, title):
+    info = books_data .get(title)
+
+    if info:
+        return HttpResponse(info)
+    else:
+        return redirect('books')
